204d5b110d8da5f307cb7f10dc2629c1bc83b58e
Adrian Gonzalez <bernardogzzf@gmail.com>
2013-12-04 20:56:08 -0600

    Initial Commit

---
+++ Wordplay/ILAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ILAppDelegate.h
+//  Wordplay
+//
+//  Created by Adrian Gonzalez on 12/4/13.
+//  Copyright (c) 2013 Adrian Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ILAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wordplay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wordplay
+//
+//  Created by Adrian Gonzalez on 12/4/13.
+//  Copyright (c) 2013 Adrian Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ILAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ILAppDelegate class]));
+    }
+}
