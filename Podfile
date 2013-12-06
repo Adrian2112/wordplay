11bbd7cec8de87cbd6efbf514a109b44647a5129
Adrian Gonzalez <bernardogzzf@gmail.com>
2013-12-05 21:53:46 -0600

    add DWTagList to display word list

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios, '7.0'
 
 pod 'Firebase', '~> 1.0.0'
+pod 'DWTagList'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// DWTagList
+#define COCOAPODS_POD_AVAILABLE_DWTagList
+#define COCOAPODS_VERSION_MAJOR_DWTagList 0
+#define COCOAPODS_VERSION_MINOR_DWTagList 0
+#define COCOAPODS_VERSION_PATCH_DWTagList 4
+
 // Firebase
 #define COCOAPODS_POD_AVAILABLE_Firebase
 #define COCOAPODS_VERSION_MAJOR_Firebase 1
